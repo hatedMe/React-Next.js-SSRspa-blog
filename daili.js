8990fa9f033d3d66f62054a4e022b0e3de142991
Atom <7548764@qq.com>
2017-10-10 23:21:05 +0800

    update file 10.10

--- daili.js
+++ daili.js
@@ -1,4 +1,3 @@
-
 /**
  * 
  * 
@@ -6,22 +5,49 @@
  */
 
 
-var Flower = function(){};
+var Flower = function() {};
 
 var xiaoming = {
-    sendFlower : function( target ){
+    sendFlower: function(target) {
         var flower = new Flower();
-        target.receiveFlower( flower )
+        target.receiveFlower(flower)
     }
 };
 
 var A = {
-    receiveFlower : function(){
+    receiveFlower: function() {
         console.log('收到花' + flower);
     }
 }
 
-xiaoming.sendFlower( A );
+xiaoming.sendFlower(A);
 
 
 
+
+
+//  ajax(...) 是某个库中提供的某个ajax函数
+var data = ajax(url);
+
+console.log(data);
+//  啊哦！data通常不会包含ajax的结果
+
+
+//  ajax(...) 是某个库中提供的某个ajax函数
+var data = ajax(url, function mycallback(data) {
+    console.log(data); //  耶！得到了一些数据
+});
+
+
+function now() {
+    return 21;
+}
+
+function later() {
+    answer = answer * 2;
+    console.log('meaning of life :', answer);
+}
+
+var answer = now();
+
+setTimeout(later, 1000); // meaning of life : 42
