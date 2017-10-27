ac922edd9e2699413c0acf245963020f4e2018b1
Atom <7548764@qq.com>
2017-10-27 17:28:55 +0800

    update file 10.27

--- muse/build/webpack.prod.conf.js
+++ muse/build/webpack.prod.conf.js
@@ -16,43 +16,43 @@ const env = process.env.NODE_ENV === 'testing'
 
 const webpackConfig = merge(baseWebpackConfig, {
   module: {
-    rules: utils.styleLoaders({
-      sourceMap: config.build.productionSourceMap,
-      extract: true
-    })
+    rules: utils.styleLoaders({sourceMap: config.build.productionSourceMap, extract: true})
   },
-  devtool: config.build.productionSourceMap ? '#source-map' : false,
+  devtool: !config.build.productionSourceMap
+    ? '#source-map'
+    : false,
   output: {
     path: config.build.assetsRoot,
-    filename: utils.assetsPath('js/[name].[chunkhash].js'),
-    chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
+    filename: utils.assetsPath('js/[name].[chunkhash:2].js'),
+    chunkFilename: utils.assetsPath('js/[id].[chunkhash:2].js')
   },
   plugins: [
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
-    new webpack.DefinePlugin({
-      'process.env': env
-    }),
-    // UglifyJs do not support ES6+, you can also use babel-minify for better treeshaking: https://github.com/babel/minify
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      },
-      sourceMap: true
-    }),
+    new webpack.DefinePlugin({'process.env': env}),
+    // UglifyJs do not support ES6+, you can also use babel-minify for better
+    // treeshaking: https://github.com/babel/minify
+    new webpack
+      .optimize
+      .UglifyJsPlugin({
+        compress: {
+          warnings: false
+        },
+        sourceMap: true
+      }),
     // extract css into its own file
     new ExtractTextPlugin({
       filename: utils.assetsPath('css/[name].[contenthash].css')
     }),
-    // Compress extracted CSS. We are using this plugin so that possible
-    // duplicated CSS from different components can be deduped.
+    // Compress extracted CSS. We are using this plugin so that possible duplicated
+    // CSS from different components can be deduped.
     new OptimizeCSSPlugin({
       cssProcessorOptions: {
         safe: true
       }
     }),
-    // generate dist index.html with correct asset hash for caching.
-    // you can customize output by editing /index.html
-    // see https://github.com/ampedandwired/html-webpack-plugin
+    // generate dist index.html with correct asset hash for caching. you can
+    // customize output by editing /index.html see
+    // https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
       filename: process.env.NODE_ENV === 'testing'
         ? 'index.html'
@@ -63,8 +63,7 @@ const webpackConfig = merge(baseWebpackConfig, {
         removeComments: true,
         collapseWhitespace: true,
         removeAttributeQuotes: true
-        // more options:
-        // https://github.com/kangax/html-minifier#options-quick-reference
+        // more options: https://github.com/kangax/html-minifier#options-quick-reference
       },
       // necessary to consistently work with multiple chunks via CommonsChunkPlugin
       chunksSortMode: 'dependency'
@@ -72,25 +71,20 @@ const webpackConfig = merge(baseWebpackConfig, {
     // keep module.id stable when vender modules does not change
     new webpack.HashedModuleIdsPlugin(),
     // split vendor js into its own file
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks: function (module) {
-        // any required modules inside node_modules are extracted to vendor
-        return (
-          module.resource &&
-          /\.js$/.test(module.resource) &&
-          module.resource.indexOf(
-            path.join(__dirname, '../node_modules')
-          ) === 0
-        )
-      }
-    }),
+    new webpack
+      .optimize
+      .CommonsChunkPlugin({
+        name: 'vendor',
+        minChunks: function (module) {
+          // any required modules inside node_modules are extracted to vendor
+          return (module.resource && /\.js$/.test(module.resource) && module.resource.indexOf(path.join(__dirname, '../node_modules')) === 0)
+        }
+      }),
     // extract webpack runtime and module manifest to its own file in order to
     // prevent vendor hash from being updated whenever app bundle is updated
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'manifest',
-      chunks: ['vendor']
-    }),
+    new webpack
+      .optimize
+      .CommonsChunkPlugin({name: 'manifest', chunks: ['vendor']}),
     // copy custom static assets
     new CopyWebpackPlugin([
       {
@@ -105,24 +99,22 @@ const webpackConfig = merge(baseWebpackConfig, {
 if (config.build.productionGzip) {
   const CompressionWebpackPlugin = require('compression-webpack-plugin')
 
-  webpackConfig.plugins.push(
-    new CompressionWebpackPlugin({
+  webpackConfig
+    .plugins
+    .push(new CompressionWebpackPlugin({
       asset: '[path].gz[query]',
       algorithm: 'gzip',
-      test: new RegExp(
-        '\\.(' +
-        config.build.productionGzipExtensions.join('|') +
-        ')$'
-      ),
+      test: new RegExp('\\.(' + config.build.productionGzipExtensions.join('|') + ')$'),
       threshold: 10240,
       minRatio: 0.8
-    })
-  )
+    }))
 }
 
 if (config.build.bundleAnalyzerReport) {
   const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
-  webpackConfig.plugins.push(new BundleAnalyzerPlugin())
+  webpackConfig
+    .plugins
+    .push(new BundleAnalyzerPlugin())
 }
 
 module.exports = webpackConfig
